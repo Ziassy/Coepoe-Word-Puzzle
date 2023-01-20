a366bf29054140453e67ce1d694a9c8d4a5e1fc9
fauziah09 <48745584+fauziah09@users.noreply.github.com>
2023-01-20 23:15:48 +0700

    [ADD] Loop level game

--- src/App.java
+++ src/App.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
 import java.util.HashSet;
+import java.util.Arrays;
 
 public class App {
+    // Global variabel
+    private static final int MIN_WORD_LENGTH = 3;
+    private static final int MAX_WORD_LENGTH = 6;
+    private static final int MIN_SCORE = 70;
+    private static final int MAX_TRIES = 10;
+
     public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        // Store word that already used with HashSet<>()
+        HashSet<String> usedWords = new HashSet<>();
         String[] array = {
                 "Coepoe Word Puzzle",
                 "=================== \n",
@@ -11,9 +22,80 @@ public class App {
                 "2. Each level, You have 10 chances to answer correctly.",
                 "3. To get through to next level, you have to score 70 points each level"
         };
+        String[] correctAnswer = { "die", "led", "lei", "let", "lied", "lie", "lit", "tie", "deli", "diet",
+                "edit", "idle", "lied", "tide", "tied", "tile", "tilt", "tilde", "tiled", "title", "tilted", "titled" };
+        String answer = "ted";
+
+        if (Arrays.asList(correctAnswer).contains(answer)) {
+            System.out.println("The word is valid.");
+        } else {
+            System.out.println("The word is not valid.");
+        }
 
         for (String teks : array) {
             System.out.println(teks);
         }
+
+        // Level 1
+        int lvl = 1;
+        int score = 0;
+        while (lvl <= 3) {
+            System.out.println("\nLevel " + lvl);
+            System.out.println("You have " + MAX_TRIES + " tries to reach a score of " + MIN_SCORE);
+
+            int tries = 0;
+            while (tries < MAX_TRIES) {
+
+                System.out.print("Enter a word: ");
+                String word = input.nextLine();
+
+                if (word.length() < MIN_WORD_LENGTH || word.length() > MAX_WORD_LENGTH) {
+                    System.out.println("Invalid input. Word must have at least " + MIN_WORD_LENGTH
+                            + " characters and at most " + MAX_WORD_LENGTH + " characters.");
+                    continue;
+                }
+                if (usedWords.contains(word)) {
+                    System.out.println("Invalid input. You've already used that word.");
+                    continue;
+                }
+                // Tambahkan kata yang digunakan ke HashSet
+                usedWords.add(word);
+
+                // Tambahkan skor jika jawaban benar
+                score += 10;
+                System.out.println("Correct! Your current score is " + score);
+
+                // Cek apakah skor sudah mencukupi untuk melanjutkan ke level berikutnya
+                if (score >= MIN_SCORE) {
+                    System.out.println(
+                            "Congratulations! You've reached the required score to move on to the next level.");
+                    lvl++;
+                    break;
+                }
+                tries++;
+            }
+
+            if (tries == MAX_TRIES && score < MIN_SCORE) {
+                System.out.print("You did not reach the required score. Do you want to try again? (Y/N): ");
+                String choice = input.nextLine();
+
+                if (choice.equalsIgnoreCase("Y")) {
+                    usedWords.clear();
+                    continue;
+                } else if (choice.equalsIgnoreCase("N")) {
+                    System.out.println("Game Over. Your final score is: " + score);
+                    return;
+                } else {
+                    System.out.println("Invalid input. Exiting game.");
+                    return;
+                }
+            }
+            // Jika user berhasil melewati level 3 dengan skor minimal 70, tampilkan nilai
+            // akhir yang diakumulasi dari level 1
+            System.out.println("Congratulations! You've completed all levels. Your final score is: " + score);
+            input.close();
+
+        }
+
     }
 }
